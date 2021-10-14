f5a3734a4d790e18307861f82746b8a69534b3cf
Nadeesha Pathiraja <pdncpathiraja95@gmail.com>
2021-10-14 14:57:54 +0530

    Add now

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
-const primaryColor = Color(0xffffc2ff);
+const primaryColor = Color(0xff309975);
+const darkPrimaryColor = Color(0xff226b5e);
+const btnColor = Color(0xff305699);
 const greyColor = Color(0xffa488b3);
-const kwhite = Colors.white;
-const fontColor = Colors.black;
-const kblue = Colors.blue;
+const kWhite = Colors.white;
+const kBlack = Colors.black;
